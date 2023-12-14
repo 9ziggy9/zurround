67a6e0bd773a260ee69e044c3336e0160902dfaf
9ziggy9 <davidarogers@protonmail.com>
2023-12-14 14:57:03 -0800

    added content entry point access and header generation

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ type Frame = {
 }
 
 interface FrameHandler {
-  frames:   {[id: Frame["id"]]: Frame};
-  newFrame: (x: Frame["x"], y: Frame["y"]) => Frame["id"];
+  frames: {[id: Frame["id"]]: Frame};
+  new:    (x: Frame["x"], y: Frame["y"]) => Frame["id"];
 };
 
 function initFrameHandler(): FrameHandler {
@@ -23,45 +23,76 @@ function initFrameHandler(): FrameHandler {
       : `frame-${Object.keys(__frames).length + 1}`;
   }
 
-  function __newFrameAtPosition(x: Frame["x"], y: Frame["y"]): Frame["id"] {
+  function __attachFloatingLogic(div: HTMLDivElement): void {
+    div.onmousedown = function(e) {
+      const dx: number = e.clientX - div.getBoundingClientRect().left;
+      const dy: number = e.clientY - div.getBoundingClientRect().top;
+      document.onmousemove = function(e) {
+        div.style.left = `${e.clientX - dx}px`;
+        div.style.top  = `${e.clientY - dy}px`;
+      };
+      document.onmouseup = function () {
+        document.onmousemove = document.onmouseup = null;
+      };
+    }
+  }
+
+  function __generateFrameHeader(headerText: string): HTMLDivElement {
+    const header = document.createElement("div");
+    if (!header) throw new Error("Failed to generate header.");
+    header.setAttribute("class", "frame-header");
+    header.innerText = headerText;
+    return header;
+  }
+
+  function __generateFrameContent(): HTMLDivElement {
+    const content = document.createElement("div");
+    if (!content) throw new Error("Failed to generate content.");
+    content.setAttribute("class", "frame-entry");
+    content.innerText = "Testing Window Content";
+    return content;
+  }
+
+  function __newFrameAtPosition(
+    x: Frame["x"],
+    y: Frame["y"],
+    headerText?: string,
+  ): Frame["id"] {
     const newFrameDiv = document.createElement("div");
-    if (!newFrameDiv) { throw new Error("Failed to create new frame."); }
+    if (!newFrameDiv) throw new Error("Failed to create new frame.");
+    __attachFloatingLogic(newFrameDiv);
     const newFrameId  = __newIdentifier();
     newFrameDiv.setAttribute("class", "frame");
     newFrameDiv.style.left = `${x}px`;
     newFrameDiv.style.top  = `${y}px`
     newFrameDiv.setAttribute("id", newFrameId);
+
+    newFrameDiv.append(
+      __generateFrameHeader(headerText || newFrameId),
+      __generateFrameContent(),
+    );
+
     __frames[newFrameId] = {
       x, y,
       id:  newFrameId,
       ref: newFrameDiv,
     };
+
+    const body = document.getElementsByTagName("body")[0];
+    body!.appendChild(newFrameDiv);
+
     return newFrameId;
   }
 
   return {
-    frames:   __frames,
-    newFrame: (x, y)  => __newFrameAtPosition(x,y),
+    frames: __frames,
+    new:    (x, y)  => __newFrameAtPosition(x,y),
   };
 }
 
 function main(): void {
-  const draggedEl = document.getElementById("frame-1") as HTMLElement;
-  if (!draggedEl) return;
-  draggedEl.onmousedown = function(e) {
-    console.log("Mouse down.");
-    const dx: number = e.clientX - draggedEl.getBoundingClientRect().left;
-    const dy: number = e.clientY - draggedEl.getBoundingClientRect().top;
-    document.onmousemove = function(e) {
-      console.log("Mouse move.");
-      draggedEl.style.left = `${e.clientX - dx}px`;
-      draggedEl.style.top  = `${e.clientY - dy}px`;
-    };
-    document.onmouseup = function () {
-      console.log("Mouse up.");
-      document.onmousemove = document.onmouseup = null;
-    };
-  }
+  const fh: FrameHandler = initFrameHandler();
+  fh.new(0,0);
 }
 
 window.onload = main;
